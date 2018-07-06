f079d83c2e3c7a54ad61e57a04e6127d9bedc23a
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-07-06 17:26:06 +0700

    [ADD] dev : create profile component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,12 @@
 
 @section('custom_head')
     <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/profile.css') }}">
 @endsection
 
 @section('content')
     @include('components.banner')
+    @include('components.profile')
 @endsection
 
 @section('custom_script')
