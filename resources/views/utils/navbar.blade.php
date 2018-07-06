b076c6f94768b62a8ca8677a513f9000bf8aa99f
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-07-06 12:03:47 +0700

    [ADD] master : create index views and navbar

---
+++ resources/views/utils/navbar.blade.php
@@ -0,0 +1,28 @@
+<section id="nav">
+    <div class="container-fluid bg-nav">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="#">
+                <img class="logo-size" src="{{ asset('imgs/logo_Wasin.svg') }}" alt="Wasin" width="100%">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link menu" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu" href="#">PROFILE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu" href="#">SKILLS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link menu" href="#">BLOG</a>
+                    </li>
+                </ul>
+            </div> 
+        </nav>
+    </div>
+</section>
