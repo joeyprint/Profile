df3203cc16538b9a8dd3968dad5ca4d9cdac08e2
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-07-06 16:52:54 +0700

    [DELETE] dev : delete font file

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Wasin Wachirapusitanun</title>
+        <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700|Prompt" rel="stylesheet">
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/default-index.css') }}">
 
