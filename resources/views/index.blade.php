15659d1ecc9ebcca1081cf38ce8c64b21339462c
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-07-09 13:02:53 +0700

    [ADD] dev : create skill component

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,13 @@
 @section('custom_head')
     <link rel="stylesheet" href="{{ asset('/css/banner.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/profile.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/skills.css') }}">
 @endsection
 
 @section('content')
     @include('components.banner')
     @include('components.profile')
+    @include('components.skills')
 @endsection
 
 @section('custom_script')
