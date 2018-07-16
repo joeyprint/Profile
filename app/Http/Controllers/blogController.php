b25531c056fac378fa52f09b6c5e851b57c79dc9
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2018-07-16 09:43:02 +0700

    [EDIT] dev : edit index to call feed

--- app/Http/Controllers/blogController.php
+++ app/Http/Controllers/blogController.php
@@ -9,7 +9,7 @@ class blogController extends Controller
     function pullBlog () {        
         $jsonUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40joeywasin';
         $data = json_decode(file_get_contents($jsonUrl));
-        $blog = $data->items[1];
+        $blog = $data->items[0];
 
         // dd($blog);
 
